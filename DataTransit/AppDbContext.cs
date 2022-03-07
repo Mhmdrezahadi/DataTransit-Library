2446f22703a449ed078a4470584000cfd08c6f44
Mohamadreza Hadi <life.math1998@gmail.com>
2022-03-08 00:33:12 +0330

    Add project files.

---
+++ DataTransit/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransit
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocaldb;Database=ExcelDb;Trusted_Connection=True");
+        }
+        public DbSet<ExcelData> ExcelDatas { get; set; }
+    }
+    public class ExcelData
+    {
+        [Key]
+        public string Id { get; set; }
+        public string data { get; set; }
+    }
+}
---
+++ DataTransit/DataTransit.cs
@@ -0,0 +1,42 @@
+﻿using GemBox.Spreadsheet;
+using StackExchange.Redis;
+
+namespace DataTransit
+{
+    public static class DataTransit
+    {
+        public static async Task AddExcelDataToRedis(FileStream file, string redisConnectionString)
+        {
+            var _redisDb = ConnectionMultiplexer.Connect(redisConnectionString).GetDatabase();
+            ExcelFile workbook = ExcelFile.Load(file);
+            ExcelWorksheet worksheet = workbook.Worksheets[0];
+            // Iterate through all rows in a worksheet.
+            foreach (ExcelRow row in worksheet.Rows)
+            {
+                string value = row.Cells[0].Value?.ToString() ?? "EMPTY";
+                await _redisDb.SetAddAsync($"ExcelData:row:{row.Index}:col:A", value, CommandFlags.FireAndForget);
+            }
+        }
+        public static async Task AddRedisDataToSql(string redisConnectionString,int dbNumber, string pattern)
+        {
+            var db = ConnectionMultiplexer.Connect(redisConnectionString).GetDatabase();
+            var server = ConnectionMultiplexer.Connect(redisConnectionString).GetServer(redisConnectionString);
+
+            var keys = server.Keys(dbNumber, pattern);
+            using (var _dbContext = new AppDbContext())
+            {
+                foreach (var key in keys)
+                {
+                    var value = await db.SetPopAsync(key);
+
+                    _dbContext.ExcelDatas.Add(new ExcelData
+                    {
+                        Id = key,
+                        data = value
+                    });
+                }
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
