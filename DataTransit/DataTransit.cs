99af8459832df75f233ec395131d181780b47aa0
Mohamadreza Hadi <life.math1998@gmail.com>
2022-03-08 02:41:14 +0330

    add duplicate condtition

--- DataTransit/DataTransit.cs
+++ DataTransit/DataTransit.cs
@@ -10,14 +10,14 @@ namespace DataTransit
             var _redisDb = ConnectionMultiplexer.Connect(redisConnectionString).GetDatabase();
             ExcelFile workbook = ExcelFile.Load(file);
             ExcelWorksheet worksheet = workbook.Worksheets[0];
-            // Iterate through all rows in a worksheet.
+            // Iterate through all rows in worksheet 0.
             foreach (ExcelRow row in worksheet.Rows)
             {
                 string value = row.Cells[0].Value?.ToString() ?? "EMPTY";
                 await _redisDb.SetAddAsync($"ExcelData:row:{row.Index}:col:A", value, CommandFlags.FireAndForget);
             }
         }
-        public static async Task AddRedisDataToSql(string redisConnectionString,int dbNumber, string pattern)
+        public static async Task AddRedisDataToSql(string redisConnectionString, int dbNumber, string pattern)
         {
             var db = ConnectionMultiplexer.Connect(redisConnectionString).GetDatabase();
             var server = ConnectionMultiplexer.Connect(redisConnectionString).GetServer(redisConnectionString);
@@ -28,12 +28,14 @@ namespace DataTransit
                 foreach (var key in keys)
                 {
                     var value = await db.SetPopAsync(key);
-
-                    _dbContext.ExcelDatas.Add(new ExcelData
+                    if (!_dbContext.ExcelDatas.Any(x => x.Id == key))
                     {
-                        Id = key,
-                        data = value
-                    });
+                        _dbContext.ExcelDatas.Add(new ExcelData
+                        {
+                            Id = key,
+                            data = value
+                        });
+                    }
                 }
                 await _dbContext.SaveChangesAsync();
             }
